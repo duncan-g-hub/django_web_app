750d4f4536c549ef623c50767b1d0c903aba3736
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-03-06 15:44:24 +0100

    Ajout de la classe Listing dans models.py et gestion des vues pour afficher le contenu de la base de données

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,14 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band, Listing
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""<h1>Hello Django!</h1>
+                        <p>Mes groupes préférés sont : </p>
+                        <ul>
+                            <li>{bands[0].name}</li>
+                            <li>{bands[1].name}</li>
+                            <li>{bands[2].name}</li>
+                        </ul>""")
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
 
 def listings(request):
-    return HttpResponse('<h1>Listings</h1> <p>Liste des annonces :</p>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""<h1>Listings</h1> 
+                        <p>Liste des annonces :</p>
+                        <ul>
+                            <li>{listings[0].title}</li>
+                            <li>{listings[1].title}</li>
+                            <li>{listings[2].title}</li>
+                        </ul>""")
 
 def contact(request):
-    return HttpResponse('<h1>Contact</h1> <p>Nous contacter</p>')
+    return HttpResponse('<h1>Contact</h1> <p>Nous contacte</p>')
