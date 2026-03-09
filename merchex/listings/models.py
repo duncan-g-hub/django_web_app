4b5a94b45177dff90705b17b0a1fef5bdb15c95a
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-03-09 11:26:27 +0100

    définition de champs supplémentaires (avec validateurs) pour les modeles

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,8 +1,31 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2025)])
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+
 
 class Listing(models.Model):
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=400)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(null=True, blank=True, validators=[MinValueValidator(1900), MaxValueValidator(2025)])
+
+    class Type(models.TextChoices):
+        records = 'R'
+        clothing = 'C'
+        posters = 'P'
+        miscellaneous = 'M'
+    type = models.fields.CharField(choices=Type.choices, max_length=2)
